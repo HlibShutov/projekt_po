56f4f6ff1c4a6892f944e5dd2330c9aee4c48c94
HlibShutov <u140_hlishu_waw@technischools.com>
2025-12-03 09:47:49 +0100

    better tests

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -6,11 +6,13 @@ def test_display_statistics(capsys):
     execution_times_base_statistics: StatisticsLogger = ExecutionTimesBaseStatistics(execution_times)
     execution_times_base_statistics.display_statistics()
     captured = capsys.readouterr()
-    assert captured.out == "Mean: 1.5\nRecords count: 3 Sum: 4.5 Min: 1.0 Max: 2.0\n1.0, 1.5, 2.0\n"
+    assert "Mean: 1.5" in captured.out 
+    assert "Records count: 3 Sum: 4.5 Min: 1.0 Max: 2.0" in captured.out
 
 def test_display_statistics_empty(capsys):
     execution_times = []
     execution_times_base_statistics: StatisticsLogger = ExecutionTimesBaseStatistics(execution_times)
     execution_times_base_statistics.display_statistics()
     captured = capsys.readouterr()
-    assert captured.out == "Mean: 0\nRecords count: 0 Sum: 0 Min: 0 Max: 0\n\n"
+    assert "Mean: 0" in captured.out 
+    assert "Records count: 0 Sum: 0 Min: 0 Max: 0" in captured.out
