432d3576ab9fa824a46dced94033b88a69994cc2
HlibShutov <u140_hlishu_waw@technischools.com>
2025-12-03 09:58:27 +0100

    more uml tests

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -1,12 +1,12 @@
+from abc import ABC
 from StatisicsLogger import StatisticsLogger
 from ExecutionTimesBaseStatistics import ExecutionTimesBaseStatistics
 
-# def test_display_statistics(capsys):
-#     execution_times = [1.0, 1.5, 2.0]
-#     execution_times_base_statistics: StatisticsLogger = ExecutionTimesBaseStatistics(execution_times)
-#     execution_times_base_statistics.display_statistics()
-#     captured = capsys.readouterr()
-#     assert captured.out == "1.0, 1.5, 2.0\n"
+def test_inherits():
+    assert issubclass(ExecutionTimesBaseStatistics, StatisticsLogger)
+
+def test_is_abstract():
+    assert issubclass(StatisticsLogger, ABC)
 
 def test_get_execution_times():
     execution_times = [1.0, 1.5, 2.0]
--- tests/test_mean.py
+++ tests/test_mean.py
@@ -1,16 +1,16 @@
 from ExecutionTimesBaseStatistics import ExecutionTimesBaseStatistics
 from StatisicsLogger import StatisticsLogger
 
-def test_display_summary(capsys):
+def test_display_mean(capsys):
     execution_times = [1.0, 1.5, 2.0]
     execution_times_base_statistics: StatisticsLogger = ExecutionTimesBaseStatistics(execution_times)
     execution_times_base_statistics.display_statistics()
     captured = capsys.readouterr()
-    assert "Records count: 3 Sum: 4.5 Min: 1.0 Max: 2.0" in captured.out
+    assert "Mean: 1.5" in captured.out 
 
-def test_display_statistics_empty_summary(capsys):
+def test_display_statistics_empty_mean(capsys):
     execution_times = []
     execution_times_base_statistics: StatisticsLogger = ExecutionTimesBaseStatistics(execution_times)
     execution_times_base_statistics.display_statistics()
     captured = capsys.readouterr()
-    assert "Records count: 0 Sum: 0 Min: 0 Max: 0" in captured.out
+    assert "Mean: 0" in captured.out 
